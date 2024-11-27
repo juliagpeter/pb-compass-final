eaf5d9b5bd4f6ec0d2f8ac190a071433505a0445
Julia Peter <juliagpeter0@gmail.com>
2024-11-27 16:52:46 -0300

    fix: findOne IDs

--- nestjs-cinema/src/tickets/tickets.service.ts
+++ nestjs-cinema/src/tickets/tickets.service.ts
@@ -28,11 +28,11 @@ export class TicketsService {
     return new Promise((resolve, reject) => {
       this.dbService.getTicketDatastore().findOne({ id }, (err, doc) => {
         if (err) reject(err);
-        if (!doc) throw new NotFoundException(`Ticket with ID ${id} not found.`);
-        resolve(doc);
+        else if (!doc) reject(new NotFoundException(`Ticket with ID ${id} not found.`));
+        else resolve(doc);
       });
     });
-  }
+  }  
 
   async update(id: string, ticket: Ticket): Promise<Ticket> {
     return new Promise((resolve, reject) => {
