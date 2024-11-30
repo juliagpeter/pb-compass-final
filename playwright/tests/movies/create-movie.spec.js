4e7f0e31eb143f77d3ddfbf2b411f980f9c8fe2d
Julia Peter <juliagpeter0@gmail.com>
2024-11-30 16:02:07 -0300

    add: validacoes no create movie

--- playwright/tests/movies/create-movie.spec.js
+++ playwright/tests/movies/create-movie.spec.js
@@ -1,24 +1,19 @@
 const { test, expect } = require('@playwright/test');
-import { generateMovie } from '../../util/generate-movie';
+const { generateMovie } = require('../../util/generate-movie');
 
-test('[POST] Criação de um filme válido e verificação dos dados retornados', async ({ request }) => {
+test('[POST] Criação de um filme válido com resposta sem corpo', async ({ request }) => {
   const movie = generateMovie();
 
   const response = await request.post('movies', { data: movie });
+
+  // Validar o status da resposta
   expect(response.status()).toBe(201);
 
-  // const responseBody = await response.json();
-  // console.log(responseBody); // Exibe a resposta para verificação
+  // Obter o corpo da resposta
+  const body = await response.body();
 
-  /* Caso o ID seja retornado na resposta, armazena ele
-  let movieId;
-  if (responseBody._id) {
-    movieId = responseBody._id;
-  }
+  // Permitir que o corpo da resposta seja vazio
+  expect(body.length).toBe(0);
 
-  // Verificar outras informações
-  expect(responseBody.title).toBe(newMovie.title);
-  expect(responseBody.description).toBe(newMovie.description);
-  expect(responseBody.launchdate).toBe(newMovie.launchdate);
-  expect(responseBody.showtimes).toEqual(newMovie.showtimes); */
+  console.log('Filme criado com sucesso, mas sem corpo na resposta.');
 });
