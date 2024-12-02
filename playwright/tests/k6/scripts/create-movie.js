b7843bd1ecd3ddd00e8789e588cf354f31fb33fd
Julia Peter <juliagpeter0@gmail.com>
2024-12-02 03:11:59 -0300

    feat: k6 create movie

---
+++ playwright/tests/k6/scripts/create-movie.js
@@ -0,0 +1,69 @@
+import http from 'k6/http';
+import { check } from 'k6';
+
+const baseUrl = 'http://127.0.0.1:3000/movies';
+
+function randomNum(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// options
+export const options = {
+    scenarios: {
+        create_movies: {
+            executor: 'constant-arrival-rate',
+            rate: 200,
+            timeUnit: '1s',
+            duration: '1s',
+            preAllocatedVUs: 0,
+            maxVUs: 200,
+            exec: 'create_movie',
+        }
+    }
+};
+
+// gerar 200 filmes
+export function setup() {
+    const movies = [];
+    for (let i = 0; i < 200; i++) {
+        const movie = {
+            title: `Filme ${i + 1}`,
+            description: `Descrição do filme ${i + 1}`,
+            launchdate: `2024-11-${randomNum(1, 30)}T16:24:39.184Z`,
+            showtimes: [
+                `${randomNum(18, 23)}:00`, 
+                `${randomNum(18, 23)}:00`
+            ],
+        };
+        movies.push(movie);
+    }
+    return movies; 
+}
+
+export default function (setupData) {
+    create_movie(setupData);
+}
+
+export function create_movie(setupData) {
+    const movieIndex = __ITER % setupData.length; 
+    const movie = setupData[movieIndex]; 
+
+    const payload = JSON.stringify({
+        title: movie.title,
+        description: movie.description,
+        launchdate: movie.launchdate,
+        showtimes: movie.showtimes,
+    });
+
+    const params = {
+        headers: { 'Content-Type': 'application/json' },
+    };
+
+    const res = http.post(baseUrl, payload, params);
+    
+    check(res, {
+        'Criação de filme com sucesso': (r) => r.status === 201,
+        'Tempo de resposta em menos de 200 milissegundos': (r) => r.timings.duration < 200,
+    });
+
+}
