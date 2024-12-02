41d4c6d08afa6638e23c62fd7bdb7c9f82e28e4f
Julia Peter <juliagpeter0@gmail.com>
2024-12-02 03:35:18 -0300

    feat: k6 get movies

---
+++ playwright/tests/k6/scripts/get-movie-details.js
@@ -0,0 +1,49 @@
+import http from 'k6/http';
+import { check } from 'k6';
+
+const baseUrl = 'http://127.0.0.1:3000/movies';
+
+
+export const options = {
+    scenarios: {
+        get_movies: {
+            executor: 'constant-arrival-rate',
+            maxVUs: 100, 
+            timeUnit: '1s',
+            duration: '1s',  
+            preAllocatedVUs: 0,
+            rate: 100, 
+            exec: 'get_movies', 
+        }
+    }
+};
+
+export function setup() {
+    const setupData = http.get(baseUrl);
+    if (setupData.status === 200) {
+        const ids = setupData.json().slice(0, 200).map(item => item._id);
+        return ids;
+    } else {
+        return null;
+    }
+}
+
+
+export default function (setupData) {
+    get_movies(setupData);
+}
+
+export function get_movies(setupData) {
+    const movieIndex = __ITER % setupData.length; 
+    const movie = setupData[movieIndex]; 
+
+    const res = http.get(`${baseUrl}/${movie}`);
+    check(res, {
+        'GET de filme concluído': (r) => r.status === 200,
+        'Tempo de resposta menor que 50ms': (r) => r.timings.duration < 50,
+    });
+
+}
+
+
+
